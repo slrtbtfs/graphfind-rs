1dcca0a0342cb8ac94b32db2702f6c46ccf78211
Benedikt Jutz <benedikt.jutz@uni-ulm.de>
2022-11-17 11:34:46 +0100

    Adressed Comments on Merge Request

--- src/file_io_backends/petgraph.rs
+++ src/file_io_backends/petgraph.rs
@@ -5,20 +5,15 @@ use std::{io::Error as IOError, fs::File};
 ///
 /// Struct to be used by clients.
 ///
+#[derive(Default)]
 pub struct JsonStableGraphReadWriter {}
 
 impl JsonStableGraphReadWriter {
-    pub fn new() -> Self {
-        JsonStableGraphReadWriter {}
-    }
-}
-
-impl Default for JsonStableGraphReadWriter {
     ///
     /// Constructs a new JsonStableGraphReadWriter struct.
     /// 
-    fn default() -> Self {
-        JsonStableGraphReadWriter::new()
+    pub fn new() -> Self {
+        JsonStableGraphReadWriter {}
     }
 }
 
@@ -42,10 +37,8 @@ where
         path: &str,
         graph: &StableGraphImpl<NodeWeight, EdgeWeight>) -> Result<(), IOError> {
         let file = File::create(path)?;
-        match serde_json::ser::to_writer(file, graph) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(IOError::new(std::io::ErrorKind::Other, e)),
-        }
+        serde_json::ser::to_writer(file, graph)
+            .map_err(|e| IOError::new(std::io::ErrorKind::Other, e))
     }
 
     ///
@@ -55,11 +48,8 @@ where
     fn deserialize_graph(&self, 
         path: &str) -> Result<Box<StableGraphImpl<NodeWeight, EdgeWeight>>, IOError> {
         let file = File::open(path)?;
-        let res: Result<StableGraphImpl<NodeWeight, EdgeWeight>, serde_json::Error> 
-            = serde_json::de::from_reader(file);
-        match res {
-            Ok(graph) => Ok(Box::new(graph)),
-            Err(e) => Err(IOError::new(std::io::ErrorKind::Other, e))
-        }
+        serde_json::de::from_reader(file)
+            .map(Box::new)
+            .map_err(|e| IOError::new(std::io::ErrorKind::Other, e))
     }
 }
--- tests/test_serde_json.rs
+++ tests/test_serde_json.rs
@@ -2,38 +2,44 @@ use std::fs::{File, self};
 
 use petgraph::{visit::{IntoNodeReferences, IntoEdgeReferences, EdgeRef}, stable_graph::StableGraph};
 use rustgql::{file_io::StableGraphReadWriter, file_io_backends, graph::Graph};
+use test_dir::{TestDir, DirBuilder};
 use crate::person_graph_types::{Person, FriendOf};
 
-/// TODO I don't want to declare this module in each test file.
 mod person_graph_types;
 
-/// File paths
-const PATH_TO_READ_AND_WRITE: &str = "./tmpfiles/path.json";
-const EMPTY_PATH: &str = "./tmpfiles/bad_path.json";
+/// File names
+const NAME_TO_READ_AND_WRITE: &str = "path.json";
+const EMPTY_FILE_NAME: &str = "empty.json";
+const MISSING_NAME: &str = "missing.json";
 
 #[test]
 fn test_serde_json_graph_read_write() {
-    // TODO I want to create this dir before I run IO tests.
-    fs::create_dir("./tmpfiles");
+    // Make a new temp directory
+    let dir = TestDir::current_rnd();
+
     let read_writer
         = file_io_backends::petgraph::JsonStableGraphReadWriter::default();
     let graph_back = person_graph_types::make_sample_graph();
 
     // Serialize graph.
-    read_writer.serialize_graph(PATH_TO_READ_AND_WRITE, &graph_back).unwrap();
+    read_writer.serialize_graph(
+        // Append file name to temp dir; make to string, expect it to be there.
+        &append_path(&dir, NAME_TO_READ_AND_WRITE),
+        &graph_back).unwrap();
     // Deserialize and pack in a Box.
     let graph: Box<dyn Graph<Person, FriendOf, _, _>> =
-        read_writer.deserialize_graph(PATH_TO_READ_AND_WRITE).unwrap();
+        read_writer.deserialize_graph(&append_path(&dir, NAME_TO_READ_AND_WRITE))
+        .unwrap();
 
     // Assert # of edges, nodes is equal.
     assert_eq!(graph_back.edge_count(), graph.edge_weights().count());
     assert_eq!(graph_back.node_count(), graph.node_weights().count());
 
-    // Iterate overc nodes, assume equivalency
+    // Iterate over nodes, assume equivalency
     for node_ref in graph_back.node_references() {
-        assert_eq!(graph.node_weight(node_ref.0).unwrap(), node_ref.1)
+        assert_eq!(graph.node_weight(node_ref.0).unwrap(), node_ref.1);
     }
-    // Iterate over edges -> depends on graph_impl.
+    // Iterate over edges, assume equivalency
     for edge_ref in graph_back.edge_references() {
         assert_eq!(graph.edge_weight(EdgeRef::id(&edge_ref)).unwrap(), edge_ref.weight());
     }
@@ -42,23 +48,32 @@ fn test_serde_json_graph_read_write() {
 /// Test that checks for read failures
 #[test]
 fn test_serde_file_not_exists() {
-    fs::create_dir("./tmpfiles");
+    let dir = TestDir::temp()
+        .create(EMPTY_FILE_NAME, test_dir::FileType::EmptyFile);
+    
     // Fail to serialize from a file that doesn't exist.
     let read_writer
         = file_io_backends::petgraph::JsonStableGraphReadWriter::default();
     let read_attempt: Result<Box<StableGraph<Person, FriendOf, _, _>>, std::io::Error>
-        = read_writer.deserialize_graph(EMPTY_PATH);
+        = read_writer.deserialize_graph
+        (&append_path(&dir, MISSING_NAME));
     let err = read_attempt.expect_err("Read attempt from missing file should fail.");
 
     // Default Rust Error to expect
     assert_eq!(err.kind(), std::io::ErrorKind::NotFound);
 
     // Create an empty file and try to deserialize from it.
-    File::create(EMPTY_PATH).unwrap();
     let read_attempt: Result<Box<StableGraph<Person, FriendOf, _, _>>, std::io::Error> =
-        read_writer.deserialize_graph(EMPTY_PATH);
+        read_writer.deserialize_graph(&append_path(&dir, EMPTY_FILE_NAME));
     let err = read_attempt.expect_err("Read from empty file should fail.");
 
     assert_eq!(err.kind(), std::io::ErrorKind::Other);
     assert!(err.into_inner().is_some());
+}
+
+fn append_path(dir: &TestDir, path: &str) -> String {
+    let buffer = dir.path(path);
+    buffer.to_str().unwrap().to_string()
+    // x.to_string()
+    // todo!()
 }
