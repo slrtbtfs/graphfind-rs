6efe74bc586b7f96f11dd415f66413130afd43c7
Tobias Guggenmos <tobias.guggenmos@uni-ulm.de>
2022-11-29 22:51:33 +0100

    Add constructor
    Signed-off-by: Tobias Guggenmos <tobias.guggenmos@uni-ulm.de>

--- src/graph_backends/filter_map.rs
+++ src/graph_backends/filter_map.rs
@@ -3,25 +3,69 @@ use std::collections::HashMap;
 use crate::graph::{self};
 
 struct FilterMap<
+    'g,
     BaseNodeWeight,
     BaseEdgeWeight,
     NodeWeight,
     EdgeWeight,
     Graph: graph::Graph<BaseNodeWeight, BaseEdgeWeight>,
 > {
-    base_graph: Graph,
+    base_graph: &'g Graph,
     node_map: HashMap<Graph::NodeRef, NodeWeight>,
     edge_map: HashMap<Graph::EdgeRef, EdgeWeight>,
 }
 
 impl<
+        'g,
+        BaseNodeWeight,
+        BaseEdgeWeight,
+        NodeWeight,
+        EdgeWeight,
+        Graph: graph::Graph<BaseNodeWeight, BaseEdgeWeight>,
+    > FilterMap<'g, BaseNodeWeight, BaseEdgeWeight, NodeWeight, EdgeWeight, Graph>
+{
+    fn new<NodeFn, EdgeFn>(graph: &'g Graph, node_fn: NodeFn, edge_fn: EdgeFn) -> Self
+    where
+        NodeFn: Fn(&'g Graph, Graph::NodeRef) -> Option<NodeWeight>,
+        EdgeFn: Fn(&'g Graph, Graph::EdgeRef) -> Option<EdgeWeight>,
+    {
+        let node_map: HashMap<Graph::NodeRef, NodeWeight> = graph
+            .nodes()
+            .filter_map(|n| {
+                let weight = node_fn(graph, n)?;
+                Some((n, weight))
+            })
+            .collect();
+
+        let edge_map: HashMap<Graph::EdgeRef, EdgeWeight> = graph
+            .edges()
+            .filter(|e| {
+                let (a, b) = graph.adjacent_nodes(*e);
+                node_map.contains_key(&a) && node_map.contains_key(&b)
+            })
+            .filter_map(|e| {
+                let weight = edge_fn(graph, e)?;
+                Some((e, weight))
+            })
+            .collect();
+
+        Self {
+            base_graph: graph,
+            node_map,
+            edge_map,
+        }
+    }
+}
+
+impl<
+        'g,
         BaseNodeWeight,
         BaseEdgeWeight,
         NodeWeight,
         EdgeWeight,
         Graph: graph::Graph<BaseNodeWeight, BaseEdgeWeight>,
     > graph::Graph<NodeWeight, EdgeWeight>
-    for FilterMap<BaseNodeWeight, BaseEdgeWeight, NodeWeight, EdgeWeight, Graph>
+    for FilterMap<'g, BaseNodeWeight, BaseEdgeWeight, NodeWeight, EdgeWeight, Graph>
 {
     type NodeRef = Graph::NodeRef;
 
