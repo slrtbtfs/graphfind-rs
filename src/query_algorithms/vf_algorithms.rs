b238458606b5fc69b3c68167bad4077fa9cb578c
Benedikt Jutz <benedikt.jutz@uni-ulm.de>
2023-01-11 16:46:12 +0100

    Matcher Algorithm stub exists.

--- src/query_algorithms/vf_algorithms.rs
+++ src/query_algorithms/vf_algorithms.rs
@@ -1,21 +1,25 @@
 use std::collections::HashMap;
 
-use crate::{graph::Graph, query::PatternGraph};
+use crate::{
+    graph::Graph,
+    query::{PatternGraph, SubgraphAlgorithm},
+};
 
 ///
 /// Implements an subgraph isomorphism algorithm based on the papers
 /// "A (Sub)Graph Isomorphism Algorithm for Matching Large Graphs"
-/// by Cordella, Foggia, Sansone, and Vento.
+/// by Cordella, Foggia, Sansone, and Vento, published in 2004.
 ///
 /// The paper referenced above calls this algorithm VF2.
 ///
 
 ///
-/// VfState defines the data structures as defined in Subsection 2.4.
+/// VfState defines the required data structures as defined in Subsection 2.4
+/// of the 2004 paper.
 ///
 struct VfState<N1RefType, N2RefType> {
     ///
-    /// size contains number of nodes to match.
+    /// size contains the number of nodes to match.
     ///
     size: usize,
     ///
@@ -30,13 +34,14 @@ struct VfState<N1RefType, N2RefType> {
 impl<N1Ref, N2Ref> VfState<N1Ref, N2Ref> {
     ///
     /// Creates a new VfState for the given pattern graph and base graph.
+    /// Initialized for each base_graph instance, to use its specific indices.
     ///
     /// ## Input:
-    /// 1. `pat_graph`, a PatternGraph with N1RefType node references.
+    /// 1. `pattern_graph`, a PatternGraph with N1RefType node references.
     /// 2. `base_graph`, any Graph with N2FerType node references.
     ///
     /// ## Output:
-    /// A VfState struct.
+    /// A VfState struct with size set, and core1 initialized.
     ///
     fn init<
         NodeWeight,
@@ -48,7 +53,7 @@ impl<N1Ref, N2Ref> VfState<N1Ref, N2Ref> {
         PatternGraphType,
         BaseGraphType,
     >(
-        pat_graph: &PatternGraphType,
+        pattern_graph: &PatternGraphType,
         base_graph: &BaseGraphType,
     ) -> VfState<N1RefType, N2RefType>
     where
@@ -59,8 +64,59 @@ impl<N1Ref, N2Ref> VfState<N1Ref, N2Ref> {
         EdgeMatcher: Fn(EdgeWeight) -> bool,
     {
         VfState {
-            size: pat_graph.nodes().count(),
+            size: pattern_graph.nodes().count(),
             core1: HashMap::new(),
         }
+    }
+
+    ///
+    /// Runs the main VF2 algorithm, based on recursive depth-first search.
+    ///
+    /// ### Parameters:
+    /// d, the current search depth.
+    ///
+    fn match_graphs_vf2(&mut self, d: usize) {
+        // When the matcher reaches d = size, add the next graph.
+        if d == self.size {
+            println!("Found a Graph.")
+        } else {
+            self.match_graphs_vf2(d + 1);
+        }
+    }
+}
+
+struct VfAlgorithm {}
+
+impl SubgraphAlgorithm for VfAlgorithm {
+    ///
+    /// Wrapper for VfState calls. Currently returns an empty vector.
+    ///
+    fn find_subgraphs<
+        NodeWeight,
+        EdgeWeight,
+        NodeMatcher,
+        EdgeMatcher,
+        N1RefType,
+        N2RefType,
+        E1RefType,
+        E2RefType,
+        PatternGraphType,
+        BaseGraphType,
+        ResultGraphType,
+    >(
+        pattern_graph: &PatternGraphType,
+        base_graph: &BaseGraphType,
+    ) -> Vec<ResultGraphType>
+    where
+        NodeMatcher: Fn(NodeWeight) -> bool,
+        EdgeMatcher: Fn(EdgeWeight) -> bool,
+        PatternGraphType:
+            PatternGraph<NodeWeight, EdgeWeight, NodeMatcher, EdgeMatcher, NodeRef = N1RefType>,
+        BaseGraphType: Graph<NodeWeight, EdgeWeight, NodeRef = N2RefType>,
+        ResultGraphType: Graph<NodeWeight, EdgeWeight>,
+    {
+        let mut m_state = VfState::<N1RefType, N2RefType>::init(pattern_graph, base_graph);
+        m_state.match_graphs_vf2(0);
+        vec![]
     }
 }
