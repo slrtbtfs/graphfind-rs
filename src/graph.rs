505a2f744a1b8c4552415c02637f4a16072327b2
Tobias Guggenmos <tobias.guggenmos@uni-ulm.de>
2023-03-14 00:09:03 +0100

    move graph helpers into graph file
    Signed-off-by: Tobias Guggenmos <tobias.guggenmos@uni-ulm.de>

--- src/graph.rs
+++ src/graph.rs
@@ -121,3 +121,25 @@ pub trait Graph<NodeWeight, EdgeWeight> {
     ///
     fn count_edges(&self) -> usize;
 }
+
+///
+/// Convenience Method to directly access the predecessor nodes of node `n` in Graph `g`.
+/// Returns an iterator over these nodes.
+///
+pub fn incoming_nodes<'a, G, N, NW: 'a, EW: 'a>(g: &'a G, n: N) -> impl Iterator<Item = N> + 'a
+where
+    G: Graph<NW, EW, NodeRef = N>,
+{
+    g.incoming_edges(n).map(|e| g.adjacent_nodes(e).0)
+}
+
+///
+/// Convenience Method to directly access the successor nodes of node `n` in Graph `g`.
+/// Returns an iterator over these nodes.
+///
+pub fn outgoing_nodes<'a, G, N, NW: 'a, EW: 'a>(g: &'a G, n: N) -> impl Iterator<Item = N> + 'a
+where
+    G: Graph<NW, EW, NodeRef = N>,
+{
+    g.outgoing_edges(n).map(|e| g.adjacent_nodes(e).1)
+}
--- src/graph_backends/mod.rs
+++ src/graph_backends/mod.rs
@@ -2,7 +2,3 @@
 /// Implementation of filter + map graph transformations of node/edge weights.
 ///
 pub mod filter_map;
-///
-/// Contains auxiliary methods independent on specific Graph implementations.
-///
-pub mod graph_helpers;
